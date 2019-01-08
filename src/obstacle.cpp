2c9ae1c32a0bee40816d6d240f3de2f822429c51
Nikola Smiljanic <nikolassmiljanic5@gmail.com>
2019-01-08 22:13:20 +0100

    added tmp image for cactus

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -16,6 +16,13 @@ Obstacle::Obstacle(int x, int y)
     qDebug() << "Sirina" << size_x;
     qDebug() << "Visina" << size_y;
     this->setRect(x, -y, size_x, size_y);
-    this->setBrush(QBrush(Qt::green));
+    QBrush q;
+//    q.setTextureImage(QImage(":/images/cactus.png"));
+    QPixmap pixmapItem(":/images/cactus.png");
+    QPixmap pixmapItems = pixmapItem.scaled(QSize(size_x,size_y),  Qt::KeepAspectRatio);
+    q.setTexture(pixmapItems);
+
+
+    this->setBrush(q);
 
 }
